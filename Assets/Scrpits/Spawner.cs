6671e2b2c53d6813679bd0d63e56b1cba1fd45be
Miguel <12miguelgalego@gmail.com>
2019-12-17 05:36:05 +0000

    Spawn
    Done

---
+++ Assets/Scrpits/Spawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject Unit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hitInfo;
+            bool hitSuccess = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo, Mathf.Infinity);
+
+            if (hitSuccess)
+            {
+                Unit = Instantiate
+                (
+                    Unit.gameObject,
+                    hitInfo.point + Vector3.up * Unit.transform.lossyScale.y,
+                    Quaternion.identity
+                );
+                
+            }
+        }
+    }
+}
